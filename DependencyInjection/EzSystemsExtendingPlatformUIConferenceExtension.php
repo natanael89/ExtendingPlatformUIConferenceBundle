42b3c15369bf51f085c769829757167b7967182f
Jacek Foremski <jacek.foremski@gmail.com>
2017-05-18 11:31:34 +0200

    Set up the configuration

--- DependencyInjection/EzSystemsExtendingPlatformUIConferenceExtension.php
+++ DependencyInjection/EzSystemsExtendingPlatformUIConferenceExtension.php
@@ -2,17 +2,21 @@
 
 namespace EzSystems\ExtendingPlatformUIConferenceBundle\DependencyInjection;
 
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ConfigurationProcessor;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration.
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class EzSystemsExtendingPlatformUIConferenceExtension extends Extension
+class EzSystemsExtendingPlatformUIConferenceExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -24,5 +28,39 @@ class EzSystemsExtendingPlatformUIConferenceExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $processor = new ConfigurationProcessor($container, 'ez_systems_extending_platform_ui_conference');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $container->prependExtensionConfig('assetic', ['bundles' => ['EzSystemsExtendingPlatformUIConferenceBundle']]);
+
+        $this->prependYui($container);
+        $this->prependCss($container);
+    }
+
+    private function prependYui(ContainerBuilder $container)
+    {
+        $container->setParameter(
+            'extending_platformui.public_dir',
+            'bundles/ezsystemsextendingplatformuiconference'
+        );
+        $yuiConfigFile = __DIR__.'/../Resources/config/yui.yml';
+        $config = Yaml::parse(file_get_contents($yuiConfigFile));
+        $container->prependExtensionConfig('ez_platformui', $config);
+        $container->addResource(new FileResource($yuiConfigFile));
+    }
+
+    private function prependCss(ContainerBuilder $container)
+    {
+        $container->setParameter(
+            'extending_platformui.css_dir',
+            'bundles/ezsystemsextendingplatformuiconference/css'
+        );
+        $cssConfigFile = __DIR__.'/../Resources/config/css.yml';
+        $config = Yaml::parse(file_get_contents($cssConfigFile));
+        $container->prependExtensionConfig('ez_platformui', $config);
+        $container->addResource(new FileResource($cssConfigFile));
     }
 }
