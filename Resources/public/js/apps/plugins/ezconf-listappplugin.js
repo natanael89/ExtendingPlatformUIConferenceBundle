7651094cb8bc612dcb6d8da27a6a1cc162ddc0a3
Jacek Foremski <jacek.foremski@gmail.com>
2017-05-25 10:35:45 +0200

    Add a route to the application

--- Resources/public/js/apps/plugins/ezconf-listappplugin.js
+++ Resources/public/js/apps/plugins/ezconf-listappplugin.js
@@ -4,6 +4,15 @@ YUI.add('ezconf-listapplugin', function (Y) {
     Y.eZConf.Plugin.ListAppPlugin = Y.Base.create('ezconfListAppPlugin', Y.Plugin.Base, [], {
         initializer: function () {
             var app = this.get('host');
+
+            app.route({
+                name: "eZConfList",
+                path: "/ezconf/list",
+                view: "dashboardView",
+                service: Y.eZ.DashboardBlocksViewService,
+                sideViews: {'navigationHub': true, 'discoveryBar': false},
+                callbacks: ['open', 'checkUser', 'handleSideViews', 'handleMainView']
+            });
         }
     }, {
         NS: 'ezconfTypeApp'
