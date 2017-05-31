9c1f92e44a1eea18d6812e5ced5dc672701ea033
Jacek Foremski <jacek.foremski@gmail.com>
2017-05-31 14:49:33 +0200

    Add the minimal Application plugin

--- Resources/public/js/apps/plugins/ezconf-listappplugin.js
+++ Resources/public/js/apps/plugins/ezconf-listappplugin.js
@@ -1,3 +1,15 @@
 YUI.add('ezconf-listapplugin', function (Y) {
+    Y.namespace('eZConf.Plugin');
 
+    Y.eZConf.Plugin.ListAppPlugin = Y.Base.create('ezconfListAppPlugin', Y.Plugin.Base, [], {
+        initializer: function () {
+            var app = this.get('host');
+        }
+    }, {
+        NS: 'ezconfTypeApp'
+    });
+
+    Y.eZ.PluginRegistry.registerPlugin(
+        Y.eZConf.Plugin.ListAppPlugin, ['platformuiApp']
+    );
 });
