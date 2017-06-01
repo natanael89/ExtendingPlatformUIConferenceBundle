707f525f3bc53db2782945459dfcecda45a66730
Jacek Foremski <jacek.foremski@gmail.com>
2017-06-01 09:14:31 +0200

    Generate the HTML fragment server side

---
+++ Controller/ListController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace EzSystems\ExtendingPlatformUIConferenceBundle\Controller;
+
+use eZ\Publish\API\Repository\Values\Content\LocationQuery;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use EzSystems\PlatformUIBundle\Controller\Controller as BaseController;
+
+class ListController extends BaseController
+{
+    public function listAction()
+    {
+        $query = new LocationQuery();
+        $query->query = new Criterion\Subtree('/1/');
+
+        return $this->render('EzSystemsExtendingPlatformUIConferenceBundle:List:list.html.twig', [
+            'results' => $this->get('ezpublish.api.service.search')->findLocations($query),
+        ]);
+    }
+}
