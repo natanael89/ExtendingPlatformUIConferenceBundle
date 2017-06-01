91129bd0604c94d129b7a5cb115b57060f980424
Jacek Foremski <jacek.foremski@gmail.com>
2017-06-01 10:25:41 +0200

    Change the view to be server side view

--- Resources/public/js/views/ezconf-listview.js
+++ Resources/public/js/views/ezconf-listview.js
@@ -1,14 +1,9 @@
 YUI.add('ezconf-listview', function (Y) {
     Y.namespace('eZConf');
 
-    Y.eZConf.ListView = Y.Base.create('ezconfListView', Y.eZ.TemplateBasedView, [], {
-        render: function () {
-            this.get('container').setHTML(
-                this.template({
-                    "name": "listView"
-                })
-            );
-            return this;
+    Y.eZConf.ListView = Y.Base.create('ezconfListView', Y.eZ.ServerSideView, [], {
+        initializer: function () {
+            this.containerTemplate = '<div class="ez-view-ezconflistview"/>';
         }
     });
 });
