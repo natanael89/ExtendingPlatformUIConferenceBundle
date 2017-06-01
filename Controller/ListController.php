d454cc5fed2109cf773b0f20001012d0591e95dc
Jacek Foremski <jacek.foremski@gmail.com>
2017-06-01 10:59:45 +0200

    Add an offset parameter in the list action

--- Controller/ListController.php
+++ Controller/ListController.php
@@ -8,10 +8,11 @@ use EzSystems\PlatformUIBundle\Controller\Controller as BaseController;
 
 class ListController extends BaseController
 {
-    public function listAction()
+    public function listAction($offset)
     {
         $query = new LocationQuery();
         $query->query = new Criterion\Subtree('/1/');
+        $query->offset = (int)$offset;
 
         return $this->render('EzSystemsExtendingPlatformUIConferenceBundle:List:list.html.twig', [
             'results' => $this->get('ezpublish.api.service.search')->findLocations($query),
