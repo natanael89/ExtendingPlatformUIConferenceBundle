dfccced8948e0e13ddae69d73560744b2964fa9b
Jacek Foremski <jacek.foremski@gmail.com>
2017-06-01 10:16:53 +0200

    Update the view service to fetch the generated HTML fragment

--- Resources/public/js/views/services/ezconf-listviewservice.js
+++ Resources/public/js/views/services/ezconf-listviewservice.js
@@ -1,7 +1,21 @@
 YUI.add('ezconf-listviewservice', function (Y) {
     Y.namespace('eZConf');
 
-    Y.eZConf.ListViewService = Y.Base.create('ezconfListViewService', Y.eZ.ViewService, [], {
+    Y.eZConf.ListViewService = Y.Base.create('ezconfListViewService', Y.eZ.ServerSideViewService, [], {
+        _load: function (callback) {
+            var uri = this.get('app').get('apiRoot') + 'list';
 
+            Y.io(uri, {
+                method: 'GET',
+                on: {
+                    success: function (tId, response) {
+                        this._parseResponse(response);
+                        callback(this);
+                    },
+                    failure: this._handleLoadFailure
+                },
+                context: this
+            });
+        }
     });
 });
