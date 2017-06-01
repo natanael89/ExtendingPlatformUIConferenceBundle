e94d8d85134c971445b7445cf7cb4668728a856d
Jacek Foremski <jacek.foremski@gmail.com>
2017-06-01 09:01:20 +0200

    Add the minimal view service

---
+++ Resources/public/js/views/services/ezconf-listviewservice.js
@@ -0,0 +1,7 @@
+YUI.add('ezconf-listviewservice', function (Y) {
+    Y.namespace('eZConf');
+
+    Y.eZConf.ListViewService = Y.Base.create('ezconfListViewService', Y.eZ.ViewService, [], {
+
+    });
+});
